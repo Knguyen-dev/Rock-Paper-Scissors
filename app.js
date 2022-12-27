e760faf6d64fd34c6e0a952ab02ae00f296a8971
Kevin Nguyen <thenoc27@gmail.com>
2022-12-27 13:20:27 -0500

    Started getting necessary DOM elements for the game

---
+++ app.js
@@ -0,0 +1,4 @@
+const playerLivesEl = document.getElementById('user-lives');
+const computerLivesEl = document.getElementById('computer-lives');
+const outputSection = document.getElementById('output-event');
+const userBtns = document.querySelectorAll('.main-btn');
